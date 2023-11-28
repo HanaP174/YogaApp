fb8a253b52fb2d379795557f798e3e6e00f6a619
Hana <pilkova.hana@gmail.com>
2023-11-28 21:43:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/yogaapp/model/Yoga.kt
@@ -0,0 +1,41 @@
+package com.example.yogaapp.model
+
+data class Category (
+    val id: Int,
+    val name: String,
+    val description: String
+)
+
+data class Pose (
+    val id: Int,
+    val categoryId: Int,
+    val englishName: String,
+    val originalName: String,
+    val description: String,
+    val benefit: String,
+    val svg: String
+)
+
+data class YogaFlowPose (
+    var order: Int,
+    var time: Int,
+    var pose: Pose
+)
+
+data class CategoryResponse (
+    val id: Int,
+    val category_name: String,
+    val category_description: String,
+    val poses: List<PoseResponse>
+)
+
+data class PoseResponse (
+    val id: Int,
+    val category_name: String,
+    val english_name: String,
+    val sanskrit_name_adapted: String,
+    val sanskrit_name: String,
+    val pose_description: String,
+    val pose_benefits: String,
+    val url_svg: String
+)
