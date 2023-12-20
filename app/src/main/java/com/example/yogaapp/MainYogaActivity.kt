df0f011b59c295329476abc9f34db4443eb0b68e
Hana <pilkova.hana@gmail.com>
2023-12-20 21:35:57 +0100

    styles clean up

--- app/src/main/java/com/example/yogaapp/MainYogaActivity.kt
+++ app/src/main/java/com/example/yogaapp/MainYogaActivity.kt
@@ -1,12 +1,12 @@
 package com.example.yogaapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
 import androidx.lifecycle.ViewModelProvider
 import com.example.yogaapp.databinding.MainActivityYogaBinding
 import com.example.yogaapp.viewmodel.YogaViewModel
@@ -23,6 +23,9 @@ class MainYogaActivity : AppCompatActivity() {
         binding = MainActivityYogaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbar.title = ""
+        binding.toolbar.setNavigationIconTint(Color.parseColor("#C47CCC"))
+
         setSupportActionBar(binding.toolbar)
 
         val app = application as YogaApplication
@@ -35,22 +38,6 @@ class MainYogaActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-////        return when (item.itemId) {
-////            R.id.action_settings -> true
-////            else -> super.onOptionsItemSelected(item)
-////        }
-//    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
